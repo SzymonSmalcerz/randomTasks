8a3312f7d850ca57815bd8c42b29c762d5326d38
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2018-03-03 14:34:01 +0100

    some easy challenges done

--- codewars/8kyuChallenges.js
+++ codewars/8kyuChallenges.js
@@ -65,3 +65,19 @@ var summation = function (num) {
   return (1+num)/2 * num;
 }
 //@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+/*
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+But can you do it in 1 line of code and without any conditionals?
+
+*/
+
+function opposite(number) {
+  return -number;
+}
