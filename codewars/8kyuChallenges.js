7c3be56b7bff0b4b662f9e95bfaaa4297444a41b
Szymon Smalcerz <smalcerzszymonn@gmail.com>
2018-03-04 12:19:51 +0100

    some 7kyu and 8kyu done

--- codewars/8kyuChallenges.js
+++ codewars/8kyuChallenges.js
@@ -4,6 +4,36 @@
 
 /*
 
+Your task is to create a function - basic_op().
+
+The function should take three arguments - operation(string/char), value1(number), value2(number). The function should return result of numbers after applying the chosen operation.
+
+Examples:
+
+basicOp('+', 4, 7)         // Output: 11
+basicOp('-', 15, 18)       // Output: -3
+basicOp('*', 5, 5)         // Output: 25
+basicOp('/', 49, 7)        // Output: 7
+
+*/
+
+function basicOp(operation, value1, value2)
+{
+  switch(operation) {
+    case "*":
+        return value1 * value2;
+    case "/":
+        return value1 / value2;
+    case "-":
+        return value1 - value2;
+    default:
+        return value1 + value2;
+  }
+}
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+/*
+
 Write function avg which calculates average of numbers in given list.
 
 */
